2ff143cd51716bbb613fca97579e16e17b58767d
adhipratama <yogaadhi@icloud.com>
2022-02-08 12:44:01 +0700

    dynamic data and ++ age on click

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('hello Vue')
+const app = Vue.createApp({
+    // template: '<h2> i am the template </h2>'
+    data(){
+        return {
+            title: 'The Final Empire',
+            author: 'John Snow',
+            age: '37'
+        }
+    }
+})
+
+app.mount('#app')
